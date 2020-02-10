fb565cca6f7f6d11c16299ec80fd1849e8a06446
sun1024 <sunnanbin@gmail.com>
2020-02-10 10:10:19 +0800

    add 21

---
+++ 21.合并两个有序链表.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (59.50%)
+ * Likes:    842
+ * Dislikes: 0
+ * Total Accepted:    177.4K
+ * Total Submissions: 298.2K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 示例：
+ * 
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+
+        ListNode prev = result;
+        while(l1 != null && l2 != null){
+            if(l1.val >= l2.val){
+                prev.next = l2;
+                l2 = l2.next;
+            }else{
+                prev.next = l1;
+                l1 = l1.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return result.next;
+    }
+}
+// @lc code=end
+
