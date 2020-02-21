d987854fd9d7ff38fe43ffeedea369a9ad65ec41
sun1024 <sunnanbin@gmail.com>
2020-02-21 16:05:23 +0800

    Add 70 循环

--- solutions/70.爬楼梯.java
+++ solutions/70.爬楼梯.java
@@ -40,15 +40,31 @@
  */
 
 // @lc code=start
+// 暴力递归
+// class Solution {
+//     public int climbStairs(int n) {
+//         if(n == 1) return 1;
+//         if(n == 2) return 2;
+//         if(n == 3) return 3;
+//         if(n == 4) return 5;
+//         if(n == 5) return 8;
+//         if(n == 6) return 13;
+//         // 多一阶可以看成(n-1)阶+1或者(n-2)阶+2
+//         return climbStairs(n-1) + climbStairs(n-2);
+//     }
+// }
+// 循环
 class Solution {
     public int climbStairs(int n) {
         if(n == 1) return 1;
-        if(n == 2) return 2;
-        if(n == 3) return 3;
-        if(n == 4) return 5;
-        if(n == 5) return 8;
-        if(n == 6) return 13;
-        return climbStairs(n-1) + climbStairs(n-2);
+        int first = 1;
+        int second = 2;
+        for(int i=3; i<=n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
     }
 }
 // @lc code=end
