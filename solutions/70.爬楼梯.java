af025fa5e39d1548d6448e7650b1fb60cef4ea04
sun1024 <sunnanbin@gmail.com>
2020-02-21 21:02:20 +0800

    Add 70 dp solutions

--- solutions/70.爬楼梯.java
+++ solutions/70.爬楼梯.java
@@ -53,7 +53,20 @@
 //         return climbStairs(n-1) + climbStairs(n-2);
 //     }
 // }
-// 循环
+// 动态规划 空间复杂度O(n) dp[n] = dp[n-1] + dp[n-2]
+// class Solution {
+//     public int climbStairs(int n) {
+//         if(n == 1) return 1;
+//         int[] dp = new int[n + 1];
+//         dp[0] = 1;
+//         dp[1] = 1;
+//         for(int i=2; i<=n; i++) {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+// 动态规划 空间复杂度O(1)
 class Solution {
     public int climbStairs(int n) {
         if(n == 1) return 1;
