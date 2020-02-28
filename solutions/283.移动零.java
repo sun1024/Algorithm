fd63e6acbb9b97f360d6b29a10d33a3daa801bd5
sun1024 <sunnanbin@gmail.com>
2020-02-28 15:06:08 +0800

    Add 283 双指针

--- solutions/283.移动零.java
+++ solutions/283.移动零.java
@@ -31,20 +31,36 @@
 
 // @lc code=start
 // 暴力
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         // 两层循环 找到0则后移 
+//         for(int i=0; i<nums.length; i++) {
+//             if(nums[i] == 0) {
+//                 int flag = 0; //判断后移过程中是否全为0
+//                 for(int j=i+1; j<nums.length; j++) {
+//                     if(nums[j] != 0) flag = 1;
+//                     nums[j-1] = nums[j];
+//                 }
+//                 nums[nums.length-1] = 0;
+//                 if(flag == 0) return; //全为0 则后移结束
+//                 if(nums[i]==0 && i!=nums.length-1) i--;
+//             }
+//         }
+//     }
+// }
+// 双指针 O(n)
 class Solution {
     public void moveZeroes(int[] nums) {
-        // 两层循环 找到0则后移 
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] == 0) {
-                int flag = 0; //判断后移过程中是否全为0
-                for(int j=i+1; j<nums.length; j++) {
-                    if(nums[j] != 0) flag = 1;
-                    nums[j-1] = nums[j];
-                }
-                nums[nums.length-1] = 0;
-                if(flag == 0) return; //全为0 则后移结束
-                if(nums[i]==0 && i!=nums.length-1) i--;
+        int p1 = 0; //p1遍历nums
+        int p2 = 0; //p2存储非0值 直接在nums上操作 可以节约空间
+        for(p1=0; p1<nums.length; p1++) {
+            if(nums[p1] != 0) {
+                nums[p2++] = nums[p1];
             }
+        }
+        //p2按顺序存完了非0值 剩下的全部置0
+        while(p2<nums.length) {
+            nums[p2++] = 0;
         }
     }
 }
