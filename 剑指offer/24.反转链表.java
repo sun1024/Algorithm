41f5eb417a876a23f53fb6d1bd13367b8eda972c
sun1024 <sunnanbin@gmail.com>
2020-03-04 09:47:44 +0800

    Add 24

---
+++ 剑指offer/24.反转链表.java
@@ -0,0 +1,20 @@
+// https://www.nowcoder.com/practice/75e878df47f24fdc9dc3e400ec6058ca?tpId=13&tqId=11168&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+// 递归
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head == null|| head.next == null) return head;
+        ListNode res = ReverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return res;
+    }
+}
