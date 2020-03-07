fa2144e8809ab1182226ca6bc50b732f45de9814
sun1024 <sunnanbin@gmail.com>
2020-03-07 15:40:27 +0800

    Add 226

---
+++ leetcode/226.翻转二叉树.java
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ *
+ * https://leetcode-cn.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (73.69%)
+ * Likes:    381
+ * Dislikes: 0
+ * Total Accepted:    58.1K
+ * Total Submissions: 78.2K
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * 翻转一棵二叉树。
+ * 
+ * 示例：
+ * 
+ * 输入：
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 2     7
+ * ⁠/ \   / \
+ * 1   3 6   9
+ * 
+ * 输出：
+ * 
+ * ⁠    4
+ * ⁠  /   \
+ * ⁠ 7     2
+ * ⁠/ \   / \
+ * 9   6 3   1
+ * 
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ * 
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// // 递归
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//         if(root == null) return null;
+//         TreeNode left = root.left;
+//         root.left = invertTree(root.right);
+//         root.right = invertTree(left);
+//         return root;
+//     }
+// }
+// 迭代
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i=0; i<size; i++) {
+                TreeNode node = queue.remove();
+                TreeNode left = node.left;
+                node.left = node.right;
+                node.right = left;
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+        }
+        return root;
+    }
+}
+// @lc code=end
+
