6b1b3e58258a4d6a82ef4a8523b7c46d9416cd77
sun1024 <sunnanbin@gmail.com>
2020-07-27 21:38:31 +0800

    Add 机器人的运动范围 bfs

--- 剑指offer/机器人的运动范围.java
+++ 剑指offer/机器人的运动范围.java
@@ -30,3 +30,41 @@ public class Solution {
         return sum;
     }
 }
+// bfs
+// bfs
+import java.util.*;
+public class Solution {
+    public int movingCount(int threshold, int rows, int cols)
+    {
+        boolean [][] visited = new boolean [rows][cols];
+        int count = 0;
+        // int[] 的写法
+        Queue<int[]> queue = new LinkedList<>();
+        int[] first = {0, 0};
+        queue.add(first);
+        while(!queue.isEmpty()) {
+            int [] temp = queue.poll();
+            int i = temp[0];
+            int j = temp[1];
+            if(i>=rows || j>= cols || sum(i, j)>threshold || visited[i][j]) continue;
+            visited[i][j] = true;
+            count++;
+            // add一个int[]写法
+            queue.add(new int[] {i+1, j});
+            queue.add(new int[] {i, j+1});
+        }
+        return count;
+    }
+    private int sum(int i, int j) {
+        int sum = 0;
+        while(i != 0) {
+            sum += i%10;
+            i = i/10;
+        }
+        while(j != 0) {
+            sum += j%10;
+            j = j/10;
+        }
+        return sum;
+    }
+}
