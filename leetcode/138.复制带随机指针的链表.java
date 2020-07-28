f3387f0ac382e4ee3ea0c457020dd48a5baee8d4
sun1024 <sunnanbin@gmail.com>
2020-07-28 16:44:11 +0800

    Add 138

---
+++ leetcode/138.复制带随机指针的链表.java
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode.cn id=138 lang=java
+ *
+ * [138] 复制带随机指针的链表
+ *
+ * https://leetcode-cn.com/problems/copy-list-with-random-pointer/description/
+ *
+ * algorithms
+ * Medium (51.40%)
+ * Likes:    332
+ * Dislikes: 0
+ * Total Accepted:    39.3K
+ * Total Submissions: 71.6K
+ * Testcase Example:  '[[7,null],[13,0],[11,4],[10,2],[1,0]]'
+ *
+ * 给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+ * 
+ * 要求返回这个链表的 深拷贝。 
+ * 
+ * 我们用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+ * 
+ * 
+ * val：一个表示 Node.val 的整数。
+ * random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 解释：给定的链表为空（空指针），因此返回 null。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * -10000 <= Node.val <= 10000
+ * Node.random 为空（null）或指向链表中的节点。
+ * 节点数目不超过 1000 。
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+// 剑指offer 复杂链表的复制 再敲一遍
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return head;
+        // map存储新旧节点对应关系
+        Map<Node, Node> map = new HashMap<>();
+        Node node = head;
+        while(node != null) {
+            Node temp = new Node(node.val);
+            map.put(node, temp);
+            node = node.next;
+        }
+        node = head;
+        while(node != null) {
+            // 连接next和random
+            Node temp = map.get(node);
+            temp.next = map.get(node.next);
+            temp.random = map.get(node.random);
+            node = node.next;
+        }
+        return map.get(head);
+    }
+}
+// @lc code=end
+
