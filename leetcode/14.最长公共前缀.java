0d64663db3baaec643eca372ca8f03d879c91a33
sun1024 <sunnanbin@gmail.com>
2020-08-06 22:38:53 +0800

    Update 14

--- leetcode/14.最长公共前缀.java
+++ leetcode/14.最长公共前缀.java
@@ -37,23 +37,25 @@
  */
 
 // @lc code=start
+// 先找到字符串的最短长度，依此进行每个字符的比较，复杂度优于正常的O(mn)
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length<=0) return "";
+        if(strs.length == 0) return "";
+        if(strs.length == 1) return strs[0];
         int min = strs[0].length();
-        for(int i=0; i<strs.length; i++){
-            if(strs[i].length()<min) min = strs[i].length();
+        for(int i=1; i<strs.length; i++) {
+            min = Math.min(min, strs[i].length());
         }
-        for(int i=0; i<strs.length-1; i++){
-            for(int j=0; j<min; j++){
-                if(strs[i].charAt(j) != strs[i+1].charAt(j)){
+        for(int i=0; i<strs.length-1; i++) {
+            for(int j=0; j<min; j++) {
+                if(strs[i].charAt(j) != strs[i+1].charAt(j)) {
+                    // min可以直接再用一次
                     min = j;
                     break;
                 }
-                    
             }
         }
-        return strs[0].substring(0, min); 
+        return strs[0].substring(0, min);
     }
 }
 // @lc code=end
